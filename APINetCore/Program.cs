ebef253e94f33b4c6373a47b78aab145bb032275
williansjaime <williansjaime@outlook.com>
2024-01-02 22:56:20 -0300

    Estrutura de documentos

--- APINetCore/Program.cs
+++ APINetCore/Program.cs
@@ -1,5 +1,19 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Teste API", Description = "Making the Teste you love", Version = "v1" });
+});
+
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+   c.SwaggerEndpoint("/swagger/v1/swagger.json", "Teste DotNet API V1");
+});
 app.MapGet("/", () => "Hello World!");
 app.Run();
--- APINetCore/obj/Debug/net8.0/APINetCore.AssemblyInfo.cs
+++ APINetCore/obj/Debug/net8.0/APINetCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APINetCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71f4fed520da160ec9dde50a84ba8ef7c8657c8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf8f31260fa4fe9d35b86fdb9e5e40aea141259")]
 [assembly: System.Reflection.AssemblyProductAttribute("APINetCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APINetCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
